6001b44c032084dadb5a8632ae4d8e8243305dc9
Sanwara <sawarakhg804@gmail.com>
2026-01-10 18:30:45 +0530

    Logger and exception implementation

---
+++ networksecurity/exception/exception.py
@@ -0,0 +1,22 @@
+import sys 
+from networksecurity.logging import logger
+
+class NetworkSecurityException(Exception):
+    def __init__(self, error_message,error_details:sys):
+        self.error_message = error_message
+        _,_,exc_tb = error_details.exc_info()
+
+        self.lineno = exc_tb.tb_lineno
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
+
+    def __str__(self):
+        return "Error occured at script [{0}] line number [{1}] error [{2}]".format(self.file_name, self.lineno, str(self.error_message))
+    
+if __name__=='__main__':
+    try:
+        logger.logging.info("Entered try block. Logs working")
+        a = 1/0
+        print("Exception occured", a)
+
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
